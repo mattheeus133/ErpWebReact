9ec0bd046d8e26186ee003d3125c15664fdeed3e
mattheeus133 <barbosa.matheus40@gmail.com>
2026-05-05 22:25:02 -0300

    ----

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
+import styled from "styled-components"
 import Header from "../components/Header"
 
+const Cards = styled.div`
+
+`
 
 function Home(){
     return(
     <div className = "relative max-w-7xl">
-      
+        <Cards>
+            <div>
+                
+            </div>
+        </Cards>
     </div>
     )
 }
