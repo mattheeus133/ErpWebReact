83658d73d3986f5fd822921164b5745f3f0741ce
mattheeus133 <barbosa.matheus40@gmail.com>
2026-05-12 22:49:11 -0300

    ----

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,8 @@ function Home(){
     <div className = "relative max-w-7xl">
         <Cards>
             <div>
-                
+                <h1>Ola bem vindo ao Erp da sua empresa
+                </h1>
             </div>
         </Cards>
     </div>
